28170fcab1723f3cdb2392380e7543622254c047
Eric Monti <emonti@matasano.com>
2009-09-17 00:39:24 -0500

    whitespace

--- lib/buby/helpers.rb
+++ lib/buby/helpers.rb
@@ -4,6 +4,7 @@ require 'uri'
 class Buby
   module HttpRequestResponseHelper
 
+
     # returns the response as a Ruby String object - returns an empty string
     # if response is nil.
     def response_str
@@ -11,6 +12,7 @@ class Buby
     end
     alias response_string response_str
     alias rsp_str response_str
+
 
     # returns an array of response headers split into header name and value. 
     # For example:
@@ -26,11 +28,13 @@ class Buby
     end
     alias rsp_headers response_headers
 
+
     # Returns the message body of the response, minus headers
     def response_body
       (@rsp_split ||= rsp_str.split(/\r?\n\r?\n/, 2))[1]
     end
     alias rsp_body response_body
+
 
     # Returns the full request as a Ruby String - returns an empty string if 
     # request is nil.
@@ -39,6 +43,7 @@ class Buby
     end
     alias request_string request_str
     alias req_str request_str
+
 
     # Returns a split array of headers. Example:
     #   [
@@ -54,16 +59,19 @@ class Buby
     end
     alias req_headers request_headers
 
+
     # Returns the request message body or an empty string if there is none.
     def request_body
       (@req_split ||= req_str.split(/\r?\n\r?\n/, 2))[1]
     end
     alias req_body request_body
 
+
     # Returns a Ruby URI object derived from the java.net.URL object
     def uri
       @uri ||= URI.parse url.to_s if not url.nil?
     end
+
 
     # one-shot method to implant ourselves onto a target object's class
     # interface in ruby. All later instances will also get 'us' for free!
@@ -72,6 +80,7 @@ class Buby
       base.class.instance_eval { include(HttpRequestResponseHelper) }
       @implanted = true
     end
+
 
     def self.implanted? ; @implanted; end
   end
