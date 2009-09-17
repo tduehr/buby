b1c45d7826ec7a97d65c07c92e4a2da9b9ec8dd3
Eric Monti <emonti@matasano.com>
2009-09-16 23:15:21 -0500

    fix typo and add aliases

--- lib/buby/helpers.rb
+++ lib/buby/helpers.rb
@@ -12,17 +12,27 @@ class Buby
     alias response_string response_str
     alias rsp_str response_str
 
+    # returns an array of response headers split into header name and value. 
+    # For example:
+    #   [ 
+    #     ["HTTP/1.1 301 Moved Permanently"],
+    #     ["Server", "Apache/1.3.41 ..."],
+    #     ...
+    #   ]
     def response_headers
       if headers=(@rsp_split ||= rsp_str.split(/\r?\n\r?\n/, 2))[0]
         @rsp_headers ||= headers.split(/\r?\n/).map {|h| h.split(/\s*:\s*/,2)}
       end
     end
+    alias rsp_headers response_headers
 
+    # returns the body of the request, minus headers
     def response_body
       (@rsp_split ||= rsp_str.split(/\r?\n\r?\n/, 2))[1]
     end
+    alias rsp_body response_body
 
-    # returns the request as a Ruby String - returns an empty string if 
+    # Returns the full request as a Ruby String - returns an empty string if 
     # request is nil.
     def request_str
       return request().nil? ? "" : ::String.from_java_bytes(request())
@@ -30,6 +40,7 @@ class Buby
     alias request_string request_str
     alias req_str request_str
 
+    #
     def request_headers
       if headers=(@req_split ||= req_str.split(/\r?\n\r?\n/, 2))[0]
         @req_headers ||= headers.split(/\r?\n/).map {|h| h.split(/\s*:\s*/,2)}
@@ -51,7 +62,7 @@ class Buby
     # interface in ruby. All later instances will also get 'us' for free!
     def self.implant(base)
       return if @implanted
-      base.class.instance_eval { include(self) }
+      base.class.instance_eval { include(HttpRequestResponseHelper) }
       @implanted = true
     end
 
@@ -59,7 +70,7 @@ class Buby
   end
 
   
-  module ScanIssue
+  module ScanIssuesHelper
     # Returns a Ruby URI object derived from the java.net.URL object
     def uri
       @uri ||= URI.parse url.to_s if not url.nil?
@@ -69,7 +80,7 @@ class Buby
     # interface in ruby. All later instances will also get 'us' for free!
     def self.implant(base)
       return if @implanted
-      base.class.instance_eval { include(self) }
+      base.class.instance_eval { include(ScanIssueHelper) }
       @implanted = true
     end
 
