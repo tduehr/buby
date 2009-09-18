e58e49095178906909a68bc68e18bdf99156f80e
Eric Monti <emonti@matasano.com>
2009-09-18 11:50:25 -0500

    oops... java_obj is not a good attr/var name

--- lib/buby/extends/buby_array_wrapper.rb
+++ lib/buby/extends/buby_array_wrapper.rb
@@ -3,37 +3,37 @@ class Buby
   class BubyArrayWrapper
     include Enumerable
 
-    attr_reader :java_obj
+    attr_reader :array_obj
 
     def initialize(obj)
-      @java_obj = obj
+      @array_obj = obj
     end
 
     def [](*args)
       if args.size == 1 and args.first.kind_of? Numeric
-        self.java_obj[args[0]]
+        self.array_obj[args[0]]
       else
         self.to_a(*args)
       end
     end
 
     def each
-      self.java_obj.size.times do |idx|
-        yield self.java_obj[idx]
+      self.array_obj.size.times do |idx|
+        yield self.array_obj[idx]
       end
     end
 
     def size
-      self.java_obj.size
+      self.array_obj.size
     end
     alias length size
 
     def first
-      return(self.java_obj[0]) if(self.size > 0)
+      return(self.array_obj[0]) if(self.size > 0)
     end
 
     def last
-      return self.java_obj[self.size - 1] if(self.size > 0)
+      return self.array_obj[self.size - 1] if(self.size > 0)
     end
 
   end
