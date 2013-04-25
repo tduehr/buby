931bf0fa057120d4de3f03ed5d5fafac42df6a21
tduehr <td@matasano.com>
2013-04-25 16:58:27 -0500

    Version bump to 1.5.1

--- lib/buby/version.rb
+++ lib/buby/version.rb
@@ -1,9 +1,9 @@
 class Buby
   module Version
-    STRING = "1.5.0.pre3"
+    STRING = "1.5.1"
     MAJOR = 1
     MINOR = 5
-    PATCH = 0
-    BUILD = "pre3"
+    PATCH = 1
+    BUILD = nil
   end
 end
